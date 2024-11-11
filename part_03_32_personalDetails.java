36ea21516660e0a4f5327e858c3f4017f6acd5f0
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2024-11-12 00:44:27 +0800

    6th Commit

---
+++ part_03_32_personalDetails.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class part_03_32_personalDetails{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String longestName = "";
+        int currentLongestNameSize = 0;
+
+        int numberOfInputs = 0;
+        double averageYear = 0;
+
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+
+            String[] inputArray = input.split(",");
+            String name = inputArray[0];
+            int nameSize = inputArray[0].length();
+            int birthYear = Integer.valueOf(inputArray[1]);
+
+            if(nameSize > currentLongestNameSize){
+                currentLongestNameSize = nameSize;
+                longestName = name;
+            }
+
+            averageYear += birthYear;
+            numberOfInputs++;
+        }
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + (1.0 * (averageYear / numberOfInputs)));
+    }
+}
