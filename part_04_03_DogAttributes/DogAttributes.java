8e4e511ef4dc38a0ce4e83318095c56964e74512
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2024-11-16 23:09:10 +0800

    9th Commit

---
+++ part_04_03_DogAttributes/DogAttributes.java
@@ -0,0 +1,13 @@
+package part_04_03_DogAttributes;
+
+import java.util.Scanner;
+
+public class DogAttributes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Follow the instructions carefully
+        // and run the tests.
+        
+    }
+}
