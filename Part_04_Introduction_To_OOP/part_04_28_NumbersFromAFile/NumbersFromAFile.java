04259713d10a6cc80ed445d26ae98ff416f508b0
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2024-12-26 02:07:11 +0800

    Docs: Change the used logic

--- Part_04_Introduction_To_OOP/part_04_28_NumbersFromAFile/NumbersFromAFile.java
+++ Part_04_Introduction_To_OOP/part_04_28_NumbersFromAFile/NumbersFromAFile.java
@@ -1,4 +1,34 @@
 package Part_04_Introduction_To_OOP.part_04_28_NumbersFromAFile;
 
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class NumbersFromAFile {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("File? ");
+        String file = scanner.nextLine();
+
+        System.out.println("Lower Bound? ");
+        int lowerBound = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Upper Bound? ");
+        int upperBound = Integer.parseInt(scanner.nextLine());
+
+        int numberCount = 0;
+
+        try(Scanner fileScanner = new Scanner(Paths.get("Part_04_Introduction_To_OOP/part_04_28_NumbersFromAFile/" + file))){
+            while(fileScanner.hasNextInt()){
+                int input = fileScanner.nextInt();
+
+                if(input >= lowerBound && input <= upperBound){
+                    numberCount++;
+                }
+            }
+            System.out.println("Numbers: " + numberCount);
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
