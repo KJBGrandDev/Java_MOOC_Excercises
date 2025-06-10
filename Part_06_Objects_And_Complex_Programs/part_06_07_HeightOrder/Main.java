e0c8cfa8e9a7293cf0a8448f56ef1289c1b22164
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-06-11 01:18:27 +0800

    Activity: part_06_07 : minor changes

--- Part_06_Objects_And_Complex_Programs/part_06_07_HeightOrder/Main.java
+++ Part_06_Objects_And_Complex_Programs/part_06_07_HeightOrder/Main.java
@@ -1,4 +1,24 @@
 package Part_06_Objects_And_Complex_Programs.part_06_07_HeightOrder;
 
 public class Main {
+    public static void main(String[] args){
+        Room room = new Room();
+        room.add(new Person("Lea", 183));
+        room.add(new Person("Kenya", 182));
+        room.add(new Person("Auli", 186));
+        room.add(new Person("Terhi", 185));
+        room.add(new Person("Nina", 172));
+
+        System.out.println("");
+        for (Person person : room.getPersons()) {
+            System.out.println(person);
+        }
+
+        System.out.println();
+        System.out.println("Shortest: " + room.take());
+        System.out.println("");
+        for (Person person : room.getPersons()) {
+            System.out.println(person);
+        }
+    }
 }
--- Part_06_Objects_And_Complex_Programs/part_06_07_HeightOrder/Room.java
+++ Part_06_Objects_And_Complex_Programs/part_06_07_HeightOrder/Room.java
@@ -1,4 +1,43 @@
 package Part_06_Objects_And_Complex_Programs.part_06_07_HeightOrder;
 
+import java.util.ArrayList;
+
 public class Room {
+    private ArrayList<Person> personList;
+
+    public Room(){
+        this.personList = new ArrayList<>();
+    }
+
+    public void add(Person person){
+        personList.add(person);
+    }
+
+    public boolean isEmpty(){
+        if(personList.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Person> getPersons(){
+        return personList;
+    }
+
+    public Person shortest(){
+        Person shortest = personList.get(0);
+
+        for(Person person: personList){
+            if(shortest.getHeight() > person.getHeight()){
+                shortest = person;
+            }
+        }
+        return shortest;
+    }
+
+    public Person take(){
+        Person shortest = shortest();
+        personList.remove(shortest);
+        return shortest;
+    }
 }
