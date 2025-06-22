56071f25b55c02a59f72269a69245221258cd0a3
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-06-22 21:40:39 +0800

    Activity: part_07_05_Searching_Linear_Binary_Search_Algorithm

---
+++ Part_07_Programming_Paradigms_Algorithms/part_07_05_Searching/Book.java
@@ -0,0 +1,25 @@
+package Part_07_Programming_Paradigms_Algorithms.part_07_05_Searching;
+
+public class Book {
+    private int id;
+    private String name;
+
+    public Book(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "(id: " + id + "; name: " + name + ")";
+    }
+
+}
