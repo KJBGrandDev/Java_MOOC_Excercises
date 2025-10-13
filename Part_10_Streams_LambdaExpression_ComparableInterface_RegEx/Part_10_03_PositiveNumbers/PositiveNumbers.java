3309b3869dde574acb6d0afb60792cf42057cfac
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-13 23:47:28 +0800

    Oct_13_'Activity'_Divisible_(LambdaExpressions_Intermediate_Terminal_Operations)

--- Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_03_PositiveNumbers/PositiveNumbers.java
+++ Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_03_PositiveNumbers/PositiveNumbers.java
@@ -1,4 +1,30 @@
 package Part_10_Streams_LambdaExpression_ComparableInterface_RegEx.Part_10_03_PositiveNumbers;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PositiveNumbers {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> newList = new ArrayList<>();
+
+        System.out.println("Input a stream of numbers, type 'end' to end");
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("end")){
+                break;
+            }
+            int inputToInt = Integer.parseInt(input);
+            newList.add(inputToInt);
+        }
+        System.out.println("Positive numbers: " + positive(newList));
+    }
+
+    public static List<Integer> positive(List<Integer> list){
+        List<Integer> positiveNumbers = list.stream().mapToInt(Integer::valueOf).filter(s -> s > 0).boxed().collect(Collectors.toCollection(ArrayList::new));
+
+        return positiveNumbers;
+    }
 }
