9dd6bce924a55d3b5040edb3c9dde115dc012144
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-13 21:58:15 +0800

    Oct_13_'Activity'_AverageOfNumbers_(Stream_LambdaExpression)

--- Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_01_AverageOfNumbers/AverageOfNumbers.java
+++ Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_01_AverageOfNumbers/AverageOfNumbers.java
@@ -1,4 +1,28 @@
 package Part_10_Streams_LambdaExpression_ComparableInterface_RegEx.Part_10_01_AverageOfNumbers;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AverageOfNumbers {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+        System.out.println("Input numbers, type 'end' to stop.");
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("end")){
+                break;
+            }
+
+            list.add(input);
+        }
+
+        double average =
+                list.stream().
+                        mapToInt(s -> Integer.parseInt(s)).
+                        average().
+                        getAsDouble();
+        System.out.printf("average of numbers: %.2f",average);
+    }
 }
