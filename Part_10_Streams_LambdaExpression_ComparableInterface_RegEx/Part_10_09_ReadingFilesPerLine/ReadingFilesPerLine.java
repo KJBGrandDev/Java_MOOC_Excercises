48c3eb8670a9d09694f6a4244a702e1dc9a75633
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-14 15:18:43 +0800

    Oct_14_'Activity'_ReadingFilesPerLine_(Streams_ForLines)

---
+++ Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_09_ReadingFilesPerLine/ReadingFilesPerLine.java
@@ -0,0 +1,29 @@
+package Part_10_Streams_LambdaExpression_ComparableInterface_RegEx.Part_10_09_ReadingFilesPerLine;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ReadingFilesPerLine {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // test the method here
+
+    }
+
+    public static List<String> read(String file){
+        List<String> list = new ArrayList<>();
+
+        try{
+            Files.lines(Paths.get(file)).forEach(list::add);
+        } catch (Exception e){
+            System.out.println("Error: " + e);
+        }
+        return list;
+    }
+}
