32c86afc7acbdace2cecbb78e3a4cc0021450cf7
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-14 20:20:05 +0800

    Oct_14_'Activity'_WageOrder_(ComparableInterface_compareTo_Method)

---
+++ Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_11_WageOrder/Human.java
@@ -0,0 +1,34 @@
+package Part_10_Streams_LambdaExpression_ComparableInterface_RegEx.Part_10_11_WageOrder;
+
+public class Human implements Comparable<Human> {
+
+    private int wage;
+    private String name;
+
+    public Human(String name, int wage) {
+        this.name = name;
+        this.wage = wage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWage() {
+        return wage;
+    }
+
+    public int compareTo(Human human){
+        if(this.wage < human.wage){
+            return -1;
+        } else if(this.wage == human.wage){
+            return 0;
+        }
+        return 1;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + wage;
+    }
+}
