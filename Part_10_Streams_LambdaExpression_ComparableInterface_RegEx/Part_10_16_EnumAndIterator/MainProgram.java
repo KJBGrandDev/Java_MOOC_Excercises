d9b4982749418fa88cb8c8ba82165c3ecd982edc
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-16 01:45:26 +0800

    Oct_16_'File'_EnumAndIterator/MainProgram_(Enum_Iterator)

--- Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_16_EnumAndIterator/MainProgram.java
+++ Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_16_EnumAndIterator/MainProgram.java
@@ -1,4 +1,18 @@
 package Part_10_Streams_LambdaExpression_ComparableInterface_RegEx.Part_10_16_EnumAndIterator;
 
 public class MainProgram {
+    public static void main(String[] args){
+        Employees university = new Employees();
+        university.add(new Person("Petrus", Education.PHD));
+        university.add(new Person("Arto", Education.HS));
+        university.add(new Person("Elina", Education.PHD));
+
+        university.print();
+
+        university.fire(Education.HS);
+
+        System.out.println("==");
+
+        university.print();
+    }
 }
