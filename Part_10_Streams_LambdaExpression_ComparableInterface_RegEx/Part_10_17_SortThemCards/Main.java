17f4eae34a80ff1d68e491e56657f6b73b8c238b
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-16 15:32:34 +0800

    "Oct_16_'Initial_Files_SortThemCards/Main_(ComparableInterface_LambdaExpression_Enum_Iterators)"

---
+++ Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_17_SortThemCards/Main.java
@@ -0,0 +1,16 @@
+package Part_10_Streams_LambdaExpression_ComparableInterface_RegEx.Part_10_17_SortThemCards;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // test your code here
+
+        Card first = new Card(2, Suit.DIAMOND);
+        Card second = new Card(14, Suit.SPADE);
+        Card third = new Card(12, Suit.HEART);
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+    }
+}
