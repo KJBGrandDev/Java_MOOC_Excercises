c6947ed3a09e5819d8c9050c0a523c9e73adbe40
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-16 16:34:22 +0800

    "Oct_16_'Method'_Hand.java_(Stream_LambdaExpression_Sorting_Comparator)"

--- Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_17_SortThemCards/Hand.java
+++ Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_17_SortThemCards/Hand.java
@@ -1,7 +1,10 @@
 package Part_10_Streams_LambdaExpression_ComparableInterface_RegEx.Part_10_17_SortThemCards;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Hand {
     private List<Card> cardList;
@@ -20,5 +23,14 @@ public class Hand {
             sb.append(i).append("\n");
         }
         System.out.print(sb.toString());
+    }
+
+    public void sort(){
+
+        List<Card> sortedList = cardList.stream().sorted(Comparator.comparing(Card::getValue)).collect(Collectors.toCollection(ArrayList::new));
+
+        for(Card i : sortedList){
+            System.out.println(i);
+        }
     }
 }
