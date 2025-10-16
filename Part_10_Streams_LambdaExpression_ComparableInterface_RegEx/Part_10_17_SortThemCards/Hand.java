31aa165e82042e2e6f88697260b7103e91a676a1
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-16 17:51:14 +0800

    "Oct_16_'Method'_Hand.java_(Stream_LambdaExpression_Sorting_Comparator)"

--- Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_17_SortThemCards/Hand.java
+++ Part_10_Streams_LambdaExpression_ComparableInterface_RegEx/Part_10_17_SortThemCards/Hand.java
@@ -1,12 +1,12 @@
 package Part_10_Streams_LambdaExpression_ComparableInterface_RegEx.Part_10_17_SortThemCards;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.stream.Collectors;
 
-public class Hand {
+public class Hand implements Comparable<Hand> {
     private List<Card> cardList;
 
     public Hand(){
@@ -26,11 +26,22 @@ public class Hand {
     }
 
     public void sort(){
-
         List<Card> sortedList = cardList.stream().sorted(Comparator.comparing(Card::getValue)).collect(Collectors.toCollection(ArrayList::new));
 
         for(Card i : sortedList){
             System.out.println(i);
         }
+    }
+
+    public int compareTo(Hand hand){
+        int thisValue = cardList.stream().mapToInt(Card::getValue).reduce(0, (a,b) -> a + b);
+        int otherValue = hand.cardList.stream().mapToInt(Card::getValue).reduce(0, (a,b) -> a + b);
+
+        if(thisValue < otherValue){
+            return -1;
+        } else if (thisValue == otherValue){
+            return 0;
+        }
+        return 1;
     }
 }
