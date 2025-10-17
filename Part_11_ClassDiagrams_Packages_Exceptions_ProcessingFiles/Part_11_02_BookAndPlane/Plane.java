2e0e5f98f19fcca23bfb7907b16a5834af49b76f
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-17 22:28:37 +0800

    "Oct_17_'File'_Plane_(ClassDiagram)"

---
+++ Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles/Part_11_02_BookAndPlane/Plane.java
@@ -0,0 +1,7 @@
+package Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles.Part_11_02_BookAndPlane;
+
+public class Plane {
+    private String ID;
+    private String model;
+    private int yearOfIntroduction;
+}
