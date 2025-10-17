33a342156e8eaf90302e849f58d1b55e2737e710
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-17 22:27:12 +0800

    "Oct_17_'Activity'_Customer_(ClassDiagram)"

---
+++ Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles/Part_11_01_Customer/Customer.java
@@ -0,0 +1,7 @@
+package Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles.Part_11_01_Customer;
+
+public class Customer {
+    private String name;
+    private String address;
+    private String email;
+}
---
+++ Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles/Part_11_01_Customer/Program.java
@@ -0,0 +1,13 @@
+package Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles.Part_11_01_Customer;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // you can test your class here
+
+    }
+}
+
