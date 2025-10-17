14b6e4df23529bebd1cfab59e4f96754b8a21e3d
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-17 23:40:05 +0800

    "Oct_17_'File'_Bot_(ClassDiagram)"

---
+++ Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles/Part_11_05_PlayerAndBot/Bot.java
@@ -0,0 +1,12 @@
+package Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles.Part_11_05_PlayerAndBot;
+
+public class Bot extends Player {
+
+    public void play(){
+        System.out.println("We are playing what?");
+    }
+
+    public void addMove(String move){
+        System.out.println("My move is invincible");
+    }
+}
