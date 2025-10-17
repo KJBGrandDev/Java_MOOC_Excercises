856f6b497fdf43d52a0e187dc554cedc77451941
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-17 22:27:32 +0800

    "Oct_17_'Activity'_BookAndPlane_(ClassDiagram)"

---
+++ Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles/Part_11_02_BookAndPlane/Book.java
@@ -0,0 +1,7 @@
+package Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles.Part_11_02_BookAndPlane;
+
+public class Book {
+    private String name;
+    private String author;
+    private int pageCount;
+}
