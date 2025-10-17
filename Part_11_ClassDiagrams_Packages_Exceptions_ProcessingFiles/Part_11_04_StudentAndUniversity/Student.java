05f44e716a0d1a6ba600045cf48ac1e08986e2b4
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-17 23:29:52 +0800

    "Oct_17_'File'_Student_(ClassDiagram)"

---
+++ Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles/Part_11_04_StudentAndUniversity/Student.java
@@ -0,0 +1,7 @@
+package Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles.Part_11_04_StudentAndUniversity;
+
+public class Student {
+    private int studentID;
+    private String name;
+    private University university;
+}
