b2b600d2786a4520766dfaf122f7c4a36eb67f46
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-17 22:50:58 +0800

    "Oct_17_'File'_Ticket_(ClassDiagram)"

---
+++ Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles/Part_11_03_ShowAndTicket/Ticket.java
@@ -0,0 +1,7 @@
+package Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles.Part_11_03_ShowAndTicket;
+
+public class Ticket {
+    private int seat;
+    private int code;
+    private Show show;
+}
