1a8bfedb83ac564be5e82516c4f088959731a357
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-17 22:51:17 +0800

    "Oct_17_'Activity'_ShowAndTicket_(ClassDiagram)"

---
+++ Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles/Part_11_03_ShowAndTicket/Program.java
@@ -0,0 +1,13 @@
+package Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles.Part_11_03_ShowAndTicket;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // you can test your classes here
+
+    }
+}
+
