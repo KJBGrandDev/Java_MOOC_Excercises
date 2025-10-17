bda82075df182d31255c78b66f20cc68efdd1a78
Kent Jason Basalan <KJB.Grand.Dev@gmail.com>
2025-10-17 23:30:05 +0800

    "Oct_17_'File'_University_(ClassDiagram)"

---
+++ Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles/Part_11_04_StudentAndUniversity/University.java
@@ -0,0 +1,8 @@
+package Part_11_ClassDiagrams_Packages_Exceptions_ProcessingFiles.Part_11_04_StudentAndUniversity;
+
+import java.util.ArrayList;
+
+public class University {
+    private String name;
+    private ArrayList<Student> studentsList;
+}
